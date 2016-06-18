4680d039819b9ed81c5a729bfec803c175f847fc
daemarco <marco_8in@hotmail.com>
2016-06-18 19:38:03 +0200

    Add files via upload

---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        /// <summary>
+        /// The StudentID property is a foreign key, and the corresponding navigation property is Student.
+        /// </summary>
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        /// <summary>
+        /// By default EF uses columns named "ID" or "ClassNameID" as default keys
+        /// </summary>
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public string EMailAddress { get; set; }
+
+        /// <summary>
+        /// This is a navigation property
+        /// </summary>
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
